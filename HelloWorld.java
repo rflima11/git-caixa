4951adad6a51825a4ef780cc2a7b3e5fb1a94fe2
rflima12 <rodolfo.lima@creditas.com>
2025-08-19 20:10:10 -0300

    feature hello world v4

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World V3 - Alterado");
+        System.out.println("Hello World V4 - Nova feature");
 
     }
 
